ae2dfa4fe1a3669da7ade24bf4b492292f1cf493
naseu <naseu@Vic-NoteBook>
2020-02-22 13:00:18 +0900

    1. 초난감 DAO 만들기 및 리팩토링

--- src/main/java/com/study/springStudy_1/App.java
+++ src/main/java/com/study/springStudy_1/App.java
@@ -1,13 +1,29 @@
 package com.study.springStudy_1;
 
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ClassNotFoundException, SQLException
     {
-        System.out.println( "Hello World!" );
+      UserDao dao = new UserDao();
+      
+      
+      User user = new User();
+      user.setId("xlzcvkj");
+      user.setName("나승후");
+      user.setPassword("1");
+      
+      dao.add(user);
+      System.out.println(user.getId()+"등록성공");
+      
+      User user2= dao.get(user.getId());
+      System.out.println(user2.getName());
+      System.out.println(user.getId()+"조회성공");
+      
     }
 }
