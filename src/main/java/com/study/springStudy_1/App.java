43d170745a159f8bf918cdca7abfe43ef25ac19f
naseu <naseu@Vic-NoteBook>
2020-02-22 15:46:06 +0900

    4-1 제어의역전 (어노테이션방법으로) 빈 생성하기 p98

--- src/main/java/com/study/springStudy_1/App.java
+++ src/main/java/com/study/springStudy_1/App.java
@@ -2,6 +2,9 @@ package com.study.springStudy_1;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -11,19 +14,20 @@ public class App
 	public static void main( String[] args ) throws ClassNotFoundException, SQLException
 	{
 		
-	  UserDao dao = new DaoFactory().userDao();
-	  
-	  User user = new User();
-	  user.setId("10101010");
-	  user.setName("나승후");
-	  user.setPassword("1");
-	  
-	  dao.add(user);
-	  System.out.println(user.getId()+"등록성공");
-	  
-	  User user2= dao.get(user.getId());
-	  System.out.println(user2.getName());
-	  System.out.println(user.getId()+"조회성공");
-	  
+  ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+  UserDao dao  = context.getBean("userDao",UserDao.class);
+  
+  User user = new User();
+  user.setId("asdj");
+  user.setName("나승후");
+  user.setPassword("1");
+  
+  dao.add(user);
+  System.out.println(user.getId()+"등록성공");
+  
+  User user2= dao.get(user.getId());
+  System.out.println(user2.getName());
+  System.out.println(user.getId()+"조회성공");
+  
 	}
 }
