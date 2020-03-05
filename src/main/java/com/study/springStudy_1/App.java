3e32a264a12b8ea8a9297684875a3170b72c01bf
naseu <naseu@Vic-NoteBook>
2020-03-05 21:55:22 +0900

    6. xml 설정을 통한 bean 사용

--- src/main/java/com/study/springStudy_1/App.java
+++ src/main/java/com/study/springStudy_1/App.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 /**
  * Hello world!
@@ -14,11 +15,29 @@ public class App
 	public static void main( String[] args ) throws ClassNotFoundException, SQLException
 	{
 		
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
-		UserDao dao  = context.getBean("userDao",UserDao.class);
-		
+		/*
+		 * AnnotationConfigApplicationContext context = new
+		 * AnnotationConfigApplicationContext(CountingDaoFactory.class); UserDao dao =
+		 * context.getBean("userDao",UserDao.class);
+		 * 
+		 * User user = new User(); user.setId("asdj213453"); user.setName("나승후");
+		 * user.setPassword("1");
+		 * 
+		 * dao.add(user); System.out.println(user.getId()+"등록성공");
+		 * 
+		 * User user2= dao.get(user.getId()); System.out.println(user2.getName());
+		 * System.out.println(user.getId()+"조회성공");
+		 * 
+		 * 
+		 * CountingConnectionMaker ccm =
+		 * context.getBean("connectionMaker",CountingConnectionMaker.class); //총 2번의
+		 * DB연결을 하였기에 가운트가 최종 2번이 나오게 된다. System.out.println(ccm.counter);
+		 */
+	
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao",UserDao.class);
 		User user = new User();
-		user.setId("asdj213453");
+		user.setId("6alsdjlk");
 		user.setName("나승후");
 		user.setPassword("1");
 		
@@ -28,12 +47,6 @@ public class App
 		User user2= dao.get(user.getId());
 		System.out.println(user2.getName());
 		System.out.println(user.getId()+"조회성공");
-		
-
-		CountingConnectionMaker ccm = context.getBean("connectionMaker",CountingConnectionMaker.class);
-		//총 2번의 DB연결을 하였기에 가운트가 최종 2번이 나오게 된다.
-		System.out.println(ccm.counter);
-	
 	
 	}
 }
