283688777c5153430de1d8f2731fb5c4989c5515
naseu <naseu@Vic-NoteBook>
2020-03-05 21:21:48 +0900

    5-1. DI 접속 카운팅 P127

--- src/main/java/com/study/springStudy_1/App.java
+++ src/main/java/com/study/springStudy_1/App.java
@@ -14,20 +14,26 @@ public class App
 	public static void main( String[] args ) throws ClassNotFoundException, SQLException
 	{
 		
-  ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-  UserDao dao  = context.getBean("userDao",UserDao.class);
-  
-  User user = new User();
-  user.setId("asdj1");
-  user.setName("나승후");
-  user.setPassword("1");
-  
-  dao.add(user);
-  System.out.println(user.getId()+"등록성공");
-  
-  User user2= dao.get(user.getId());
-  System.out.println(user2.getName());
-  System.out.println(user.getId()+"조회성공");
-  
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+		UserDao dao  = context.getBean("userDao",UserDao.class);
+		
+		User user = new User();
+		user.setId("asdj213453");
+		user.setName("나승후");
+		user.setPassword("1");
+		
+		dao.add(user);
+		System.out.println(user.getId()+"등록성공");
+		
+		User user2= dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user.getId()+"조회성공");
+		
+
+		CountingConnectionMaker ccm = context.getBean("connectionMaker",CountingConnectionMaker.class);
+		//총 2번의 DB연결을 하였기에 가운트가 최종 2번이 나오게 된다.
+		System.out.println(ccm.counter);
+	
+	
 	}
 }
