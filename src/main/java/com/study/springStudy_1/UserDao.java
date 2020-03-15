48a8ecd36adb21b36541948219224fb68de0feb1
naseu <naseu@Vic-NoteBook>
2020-03-15 19:44:27 +0900

    3. 재커밋빠진 파일 추가

--- src/main/java/com/study/springStudy_1/UserDao.java
+++ src/main/java/com/study/springStudy_1/UserDao.java
@@ -16,6 +16,25 @@ public class UserDao {
 	}
 
 	public void add(User user) throws  SQLException {
+		class AddStatement implements StatementStrategy{
+			
+			User user;
+
+			public AddStatement(User user) {
+				super();
+				this.user = user;
+			}
+
+			@Override
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+
+				PreparedStatement ps = c.prepareStatement("INSERT INTO USERTB VALUES(?,?,?)");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+				return ps;
+			}
+		}
 		//선정한 전략 클래스의 오브젝트 생성
 		StatementStrategy st = new AddStatement(user);
 		//컨텍스트 호출 전략 오브젝트 전달
