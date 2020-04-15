cf422ecf0979e5206112c0088f8f347b45811c89
naseu <naseu@Vic-NoteBook>
2020-04-15 17:13:40 +0900

    5장 수정 메소드를 좀더 보완해보자

--- src/test/java/com/study/springStudy_1/userDaoTest.java
+++ src/test/java/com/study/springStudy_1/userDaoTest.java
@@ -67,7 +67,8 @@ public class userDaoTest {
 	public void update() throws SQLException, ClassNotFoundException {
 		dao.deleteAll();
 		
-		dao.add(user1);
+		dao.add(user1); //수정할 사용자
+		dao.add(user2); //수정하지 않을 사용자
 		
 		user1.setId("test7");
 		user1.setName("반세기");
@@ -75,11 +76,15 @@ public class userDaoTest {
 		user1.setLevel(User.Level.GOLD);
 		user1.setLogin(3);
 		user1.setRecommend(42);
+		
 		dao.update(user1);
+		
+		
 		
 		User user2 = dao.get(user1.getId());
 		checkSameUser(user1, user2);
-		
+		User user2same = dao.get(user2.getId());
+		checkSameUser(user2,user2same);
 	}
 	
 	
