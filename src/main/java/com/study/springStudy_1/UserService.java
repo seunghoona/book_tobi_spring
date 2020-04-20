b88f412f0e934c3660180c5ab0e61698656364bc
seunghoo <naseunghoo@naver.com>
2020-04-20 22:33:25 +0900

    5장 트랜잭션 동기화

--- src/main/java/com/study/springStudy_1/UserService.java
+++ src/main/java/com/study/springStudy_1/UserService.java
@@ -1,26 +1,35 @@
 package com.study.springStudy_1;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import com.study.springStudy_1.User.Level;
 
 public class UserService {
+	
+
+	public static final int MIN_LOGCOUNT_FOR_SILVER = 50 ;
+	public static final int MIN_RECOMMEND_FOR_GOLD  = 30 ;
+	
 	@Autowired
 	UserDao userDao;
 	
 	@Autowired
 	UserLevelUpgradePolicy userLevelUpgradePolicy;
 	
-	public static final int MIN_LOGCOUNT_FOR_SILVER = 50 ;
-	public static final int MIN_RECOMMEND_FOR_GOLD  = 30 ;
+	private DataSource dataSource;
 	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
@@ -30,13 +39,28 @@ public class UserService {
 		this.userLevelUpgradePolicy = userLevelUpgradePolicy;
 	}
 	
-	public void upgradeLevels() {
-		List<User> users = userDao.getAll();
-		for(User user: users) {
-			//
-			if(userLevelUpgradePolicy.canUpgradeLevel(user)) {
-				upgradeLevel(user);
+	public void upgradeLevels() throws SQLException {
+		TransactionSynchronizationManager.initSynchronization();
+		Connection c = DataSourceUtils.getConnection(dataSource);
+		c.setAutoCommit(false);
+		try {
+			List<User> users = userDao.getAll();
+			for(User user: users) {
+				//
+				if(userLevelUpgradePolicy.canUpgradeLevel(user)) {
+					upgradeLevel(user);
+				}
 			}
+		}catch (Exception e) {
+			c.rollback();
+			throw e;
+			// TODO: handle exception
+		}finally {
+			//DB 커넥션을 안전하게 닫느다.
+			DataSourceUtils.releaseConnection(c, dataSource);
+			//동기화작업 종료 및 정리 
+			TransactionSynchronizationManager.unbindResource(this.dataSource);
+			TransactionSynchronizationManager.clearSynchronization();
 		}
 	}
 
